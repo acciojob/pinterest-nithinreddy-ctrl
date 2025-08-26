dc7fb8ada9c0ad51f1920c88ea008237c4525b1a
nithinreddy-ctrl <reddykothapunithin@gmail.com>
2025-08-26 14:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+const cta = document.querySelector(".cta");
+cta.addEventListener("click", (e) => {
+if (cta.getAttribute("href") === "#explore") {
+e.preventDefault();
+window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+}
+});
+});
